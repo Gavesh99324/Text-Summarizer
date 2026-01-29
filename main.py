7b31f0f9fe5049a130da4b487ed86dfdd486fc32
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2026-01-29 22:30:39 +0530

    update pipeline

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger
+
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -22,3 +24,13 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
